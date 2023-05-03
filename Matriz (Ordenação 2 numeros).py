adeebb0d6e440a4d4b15fbbf91bd2952f13dd7cc
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:18:33 -0300

    Update Matriz (Ordenação 2 numeros).py

--- Matriz (Ordenação 2 numeros).py
+++ Matriz (Ordenação 2 numeros).py
@@ -1,9 +1,10 @@
 print('\033[34mMatriz (Ordenação 2 numeros)\033[m')
 print()
 
-l = []
-for v in range(2):
-    n = int(input('Digite um número inteiro: '))
-    l.append(n)
-    l.sort()
-print(l)
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+print([n1, n2])
