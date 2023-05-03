bd11f5aee7aa7161558fe2de6b2f3c25f0ebc6c8
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:33:05 -0300

    Update comp3.py

--- comp3.py
+++ comp3.py
@@ -1,63 +1,33 @@
 from No import No
 
-
 class ListaNos:
-    raiz = 0
-    texto = ''
-
     def __init__(self, lista):
         self.texto = lista
-
-    def insereRaiz(self, novo):  # insere o primeiro elemento da lista
-        self.raiz = [novo]
-        return
-
-    def insere(self, novo):  # insere o elemento no fim da lista
-
+        self.raiz = None
+    
+    def insere(self, novo):
+        """Insere o nó na lista, incrementando a frequência caso o caractere já exista na lista."""
         for item in self.raiz:
-            if item.conteudo == novo.conteudo:  # se houver outro elemento na lista com o mesmo caractere
-                item.freq += 1  # a frequência é incrementada
-                self.raiz = sorted(self.raiz, key=lambda no: no.freq)  # e a lista reordenada
-                return  # e finaliza o método
-
-        self.raiz += [novo]  # se não, o novo objeto é inserido
-        self.raiz = sorted(self.raiz, key=lambda no: no.freq)  # e a lista reordenada
-
-        return
-
+            if item.conteudo == novo.conteudo:
+                item.freq += 1
+                self.raiz.sort(key=lambda no: no.freq)
+                return
+        self.raiz.append(novo)
+        self.raiz.sort(key=lambda no: no.freq)
+        
     def criaLista(self):
+        """Cria uma lista de nós a partir do texto."""
+        self.raiz = []
         for letter in self.texto:
-            if self.raiz == 0:  # se a lista está vazia
-                # insere o primeiro elemento
-
-                self.insereRaiz(No(letter, 1, '', None, None))  # para guardar o caractere em si
-
-            # self.insereRaiz(No(str(ord(letter)),1,'', None, None)) # para inserir o valor em ASCII do caractere
-            else:
-                # se não, insere o elemento nomalmente
-                self.insere(No(letter, 1, '', None, None))  # para guardar o caractere em si
-
-            # self.insere(No(str(ord(letter)),1,'',None,None)) # para inserir o valor em ASCII do caractere
-        return
-
-    def printLista(self):  # exibe cada elemento separadamente
-        for tup in self.raiz:
-            print(tup)
-        return
-
+            self.insere(No(letter, 1, '', None, None))
+    
     def criaArv(self):
-
-        self.criaLista()  # chama o método para a criar a lista
-
-        while len(self.raiz) > 1:  # esse laço executa até restar apenas um elemento na lista, que será a raiz da árvore
-
-            novo = No("", self.raiz[0].freq + self.raiz[1].freq, '', self.raiz[0],
-                      self.raiz[1])  # cria o nó intermediário
-
-            del self.raiz[0]  # deleta os dois primeros elementos
-            del self.raiz[0]
-
-            self.raiz += [novo]  # insere o nó intermediário na lista
-            self.raiz = sorted(self.raiz, key=lambda no: no.freq)  # reordena a lista
-
-        return
+        """Cria uma árvore de Huffman a partir da lista de nós."""
+        self.criaLista()
+        while len(self.raiz) > 1:
+            novo = No("", self.raiz[0].freq + self.raiz[1].freq, '', self.raiz[0], self.raiz[1])
+            self.raiz.pop(0)
+            self.raiz.pop(0)
+            self.raiz.append(novo)
+            self.raiz.sort(key=lambda no: no.freq)
+        self.raiz = self.raiz[0]
