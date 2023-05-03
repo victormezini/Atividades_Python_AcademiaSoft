1b11078f00ed240134290fc40cf305471b53d6cc
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:10:02 -0300

    Update Cálculo de salário.py

--- Cálculo de salário.py
+++ Cálculo de salário.py
@@ -1,11 +1,13 @@
 print('\033[7mCálculo de salário\033[m')
 
-htrabalhadas=int(input('Horas trabalhadas no mês: '))
+horas_trabalhadas = int(input('Horas trabalhadas no mês: '))
+valor_hora = float(input('Valor da hora trabalhada: '))
+perc_desconto = float(input('Percentual de desconto: '))
 
-valortrabalhada=int(input('Valor da hora trabalhada: '))
+salario_bruto = horas_trabalhadas * valor_hora
+desconto = salario_bruto * perc_desconto / 100
+salario_liquido = salario_bruto - desconto
 
-percdesconto=int(input('Percentual de desconto: '))
-
-SalarioBruto = htrabalhadas * valortrabalhada
-
-print('Salario bruto: ', htrabalhadas * valortrabalhada)
+print(f'Salário bruto: R${salario_bruto:.2f}')
+print(f'Desconto: R${desconto:.2f}')
+print(f'Salário líquido: R${salario_liquido:.2f}')
