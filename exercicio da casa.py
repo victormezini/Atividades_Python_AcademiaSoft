7d4ae0fa24012e8b2fe1cb5cdd7f272b06384d06
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:39:42 -0300

    Update exercicio da casa.py

--- exercicio da casa.py
+++ exercicio da casa.py
@@ -1,18 +1,21 @@
 import pygame
+
+# inicializa o Pygame
 pygame.init()
+
+# cria a janela
 tela = pygame.display.set_mode((800, 600), 0, 32)
 
-#tela.set_at((400,300), (255, 255, 0))
-pygame.draw.line(tela, (255, 255, 0), (400,100), (500, 160), 3)
-pygame.draw.line(tela, (255, 255, 0), (500,160), (300, 160), 3)
-pygame.draw.line(tela, (255, 255, 0), (300,160), (400, 100), 3)
-pygame.draw.line(tela, (255, 255, 0), (500, 160), (500, 340), 3)
-pygame.draw.line(tela, (255, 255, 0), (500, 340), (300, 340), 3)
-pygame.draw.line(tela, (255, 255, 0), (300, 340), (300, 160), 3)
+# desenha o hexágono
+pontos = [(400, 100), (500, 160), (500, 340), (400, 400), (300, 340), (300, 160)]
+pygame.draw.polygon(tela, (255, 255, 0), pontos, 3)
 
+# atualiza a tela
 pygame.display.update()
 
+# loop principal do jogo
 while True:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            exit()      
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            exit()
