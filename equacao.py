eed8c3d7a505119a70fd1cf0c07c2ba2350b5774
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:38:09 -0300

    Update equacao.py

--- equacao.py
+++ equacao.py
@@ -1,25 +1,30 @@
+from math import sqrt
+
 print('\033[1mPrograma de equação de segundo grau\033[m')
-nome=str(input('Digite o seu nome: '))
-ra=int(input('Digite o seu RA: '))
-programa=str(input('Coloque o nome do programa: '))
-print('*********************************************************************************************')
+nome = input('Digite o seu nome: ')
+ra = int(input('Digite o seu RA: '))
+programa = input('Coloque o nome do programa: ')
 
-print('******    Programação II -  2º Ciclo Jogos Digitais                                   ******')
+print('*' * 93)
+print(f'******    Programação II - 2º Ciclo Jogos Digitais {" " * 30}******')
+print(f'******    Nome: {nome}{" " * (39 - len(nome))} RA: {ra}{" " * (22 - len(str(ra)))}******')
+print(f'******    Programa: {programa}{" " * (40 - len(programa))}******')
+print('*' * 93)
 
-print('******   Nome:' ,nome,'           RA:' ,ra,'                ******')
+a = float(input('Digite o valor de A: '))
+b = float(input('Digite o valor de B: '))
+c = float(input('Digite o valor de C: '))
 
-print('******   Programa :' ,programa,'                                        ******')
+delta = b ** 2 - 4 * a * c
+raizdelta = sqrt(delta)
 
-print('*********************************************************************************************')
-
-a= float(input('Digite o valor de A='))
-b= float(input('Digite o valor de B='))
-c= float(input('Digite o valor de C='))
-delta = b**2-4*a*c
-print('Delta=', delta)
-raizdelta=delta**0.5
-print('Raiz de delta= ', raizdelta)
-x1=(-b+raizdelta)/(2*a)
-print('x1 = ', x1)
-x2=(-b-raizdelta)/(2*a)
-print('x2 = ', x2)
+if delta >= 0:
+    x1 = (-b + raizdelta) / (2 * a)
+    x2 = (-b - raizdelta) / (2 * a)
+    print(f'x1 = {x1:.2f}')
+    print(f'x2 = {x2:.2f}')
+else:
+    parte_real = -b / (2 * a)
+    parte_imaginaria = abs(raizdelta) / (2 * a)
+    print(f'x1 = {parte_real:.2f} + {parte_imaginaria:.2f}i')
+    print(f'x2 = {parte_real:.2f} - {parte_imaginaria:.2f}i')
