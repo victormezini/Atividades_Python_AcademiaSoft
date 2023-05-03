c7f46ff256cf831dbeac944f3c0f239a814f1141
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:02:33 -0300

    Update 5 ao 75 e soma total.py

--- 5 ao 75 e soma total.py
+++ 5 ao 75 e soma total.py
@@ -1,12 +1,10 @@
-print('\033[34m 5 ao 75 e a soma total \033[m')
-print()
-print('\033[32m Número ______  soma total\033[m')
-print()
+print('\033[34m5 ao 75 e a soma total\033[m\n')
+print('\033[32mNúmero   Soma Total\033[m\n')
 
-n1 = 5
-n2 = 5
+numero = 5
+soma_total = 5
 
-while n1 <= 75:
-    print('{} -------------- {}'.format(n1, n2))
-    n2 += n1 + 1
-    n1 += 1
+while numero <= 75:
+    print(f'{numero:<8}-------------- {soma_total}')
+    numero += 1
+    soma_total += numero
