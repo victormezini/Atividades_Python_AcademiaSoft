ae096a3470ec8b83984418e60054d428beccb7d7
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:13:47 -0300

    Update Matriz (20 números com soma e média aritmética).py

--- Matriz (20 números com soma e média aritmética).py
+++ Matriz (20 números com soma e média aritmética).py
@@ -6,21 +6,9 @@ for v in range(20):
     n = int(input('Digite um número inteiro:'))
     l.append(n)
 
-s1 = n
-s2 = n
-
-m = (n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n) / 4
-
-while s1 <=n:
- s2 += s1 + 1
- s1 += 1
-
-#s = (n + n + n + n + n + n + n + n + n + n + n + n + n + n + n + n + n + n + n + n)
-
-#m = (n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n) / 4
+soma = sum(l)
+media = soma / len(l)
 
 print('Números ------------- Soma -------------- Média')
-
 print()
-
-print('\n{} -------------- {} -------------- {}'.format(l, s2, m))
+print('{} -------------- {} -------------- {}'.format(l, soma, media))
