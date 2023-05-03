c565b135ae1f5e6476cc05fb26e1fe4397c9019b
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:00:07 -0300

    Update 1 ao 200 raiz quadrada.py

--- 1 ao 200 raiz quadrada.py
+++ 1 ao 200 raiz quadrada.py
@@ -1,10 +1,7 @@
-print('\033[34m 1 ao 200 e a raiz quadrada \033[m')
-
 import math
 
-print()
-print('\033[32m Número ______  raiz\033[m')
-print()
+print('\033[34m1 ao 200 e a raiz quadrada\033[m\n')
+print('\033[32mNúmero   Raiz\033[m\n')
 
-for nu in range(1, 201, 1):
-    print('{} -------------- {}' .format(nu, math.sqrt(nu)))
+for nu in range(1, 201):
+    print(f'{nu:<8}-------------- {math.sqrt(nu):.2f}')
