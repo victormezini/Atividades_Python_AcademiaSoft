02a2f2643d4fba53be4ee6f0f0b5e272c71c3d76
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:43:28 -0300

    Update exercicio_jogo_21.py

--- exercicio_jogo_21.py
+++ exercicio_jogo_21.py
@@ -2,12 +2,12 @@ print('Conselheiro do jogo de 21')
 pontos = int(input('Digite o seu ponto: '))
 
 if pontos > 21:
-    print('você perdeu')
+    print('Você perdeu')
 elif pontos == 21:
-    print('Parabens você ganho')
-elif pontos > 15 and pontos <= 20:
+    print('Parabéns você ganhou!')
+elif 15 < pontos <= 20:
     print('Aconselho a parar')
-elif pontos <= 15 and pontos >= 10:
-    print('Há um risco mas aconselho a comprar mais uma carta')
+elif 10 <= pontos <= 15:
+    print('Há um risco, mas aconselho a comprar mais uma carta')
 else:
     print('Compre mais uma carta')
