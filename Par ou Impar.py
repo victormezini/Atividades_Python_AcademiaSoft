8382c222ba57ddcb9d08403f900a0b13e8fe8ce1
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:20:15 -0300

    Update Par ou Impar.py

--- Par ou Impar.py
+++ Par ou Impar.py
@@ -2,7 +2,4 @@ print('\033[7mPar ou Impar\033[m')
 
 num = int(input('Digite um número: '))
 
-if int(num) % 2 == 0:
-    print('O número é par')
-else:
-    print('O número é impar')
+print('O número é par' if num % 2 == 0 else 'O número é ímpar')
