3b9f1f62f8616e407c9a3f87e4967d2a43cb6a12
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:27:13 -0300

    Update area do retangulo.py

--- area do retangulo.py
+++ area do retangulo.py
@@ -1,7 +1,8 @@
 print('\033[7mArea de um retangulo\033[m')
-base = int(input('Digite a base do retangulo: '))
-altura = int(input('Digite a altura do retangulo: '))
 
-area = float(base) * float(altura)
+base = float(input('Digite a base do retangulo: '))
+altura = float(input('Digite a altura do retangulo: '))
 
-print ('Area = ', area)
+area = base * altura
+
+print('Area =', area)
