05b285ab244cca94df4795708e5ce8c8fca60af6
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:15:44 -0300

    Update Matriz (20 números ordem inversa).py

--- Matriz (20 números ordem inversa).py
+++ Matriz (20 números ordem inversa).py
@@ -1,10 +1,12 @@
-print('\033[34mMatriz (20 números ordem inversa)\033[m')
+print('\033[34mMatriz (20 números em ordem inversa)\033[m')
 print()
 
 l = []
-while v in range(20):
-    n = int(input('Digite um número inteiro:'))
+
+for v in range(20):
+    n = int(input('Digite um número inteiro: '))
     l.append(n)
+
 l.reverse()
 
 print(l)
