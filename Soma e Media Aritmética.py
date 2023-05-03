32ab7e853a76f7d47209f6af328aff182e4c5729
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:24:00 -0300

    Update Soma e Media Aritmética.py

--- Soma e Media Aritmética.py
+++ Soma e Media Aritmética.py
@@ -1,51 +1,13 @@
-print('\033[34m Soma e Media Aritmética\033[m')
-print()
+print('\033[34m Soma e Média Aritmética\033[m')
 
-v1 = int(input('Digite um valor: '))
+numeros = []
+for i in range(20):
+    n = int(input(f'Digite o {i+1}º valor: '))
+    numeros.append(n)
 
-v2 = int(input('Digite um valor:  '))
-
-v3 = int(input('Digite um valor:  '))
-
-v4 = int(input('Digite um valor: '))
-
-v5 = int(input('Digite um valor: '))
-
-v6 = int(input('Digite um valor: '))
-
-v7 = int(input('Digite um valor: '))
-
-v8 = int(input('Digite um valor: '))
-
-v9 = int(input('Digite um valor: '))
-
-v10 = int(input('Digite um valor: '))
-
-v11 = int(input('Digite um valor: '))
-
-v12 = int(input('Digite um valor: '))
-
-v13 = int(input('Digite um valor: '))
-
-v14 = int(input('Digite um valor: '))
-
-v15 = int(input('Digite um valor: '))
-
-v16 = int(input('Digite um valor: '))
-
-v17 = int(input('Digite um valor: '))
-
-v18 = int(input('Digite um valor: '))
-
-v19 = int(input('Digite um valor: '))
-
-v20 = int(input('Digite um valor: '))
+soma = sum(numeros)
+media = soma / len(numeros)
 
 print()
-
-soma = (v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19 + v20)
-media = (v1 * v2 * v3 * v4 * v5 * v6 * v7 * v8 * v9 * v10 * v11 * v12 * v13 * v14 * v15 * v16 * v17 * v18 * v19 * v20) / 4
-
-print('A soma é: ', (v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19 + v20))
-print()
-print('A média aritimetrica é: ', (v1 * v2 * v3 * v4 * v5 * v6 * v7 * v8 * v9 * v10 * v11 * v12 * v13 * v14 * v15 * v16 * v17 * v18 * v19 * v20) / 4)
+print(f'A soma é: {soma}')
+print(f'A média aritmética é: {media:.2f}')
