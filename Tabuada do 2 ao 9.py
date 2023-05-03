cb9f10c57866ada47ba2cc0006d810f93de5b7ae
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:24:53 -0300

    Update Tabuada do 2 ao 9.py

--- Tabuada do 2 ao 9.py
+++ Tabuada do 2 ao 9.py
@@ -1,6 +1,6 @@
 print('\033[7mTabuada do 2 ao 9\033[m')
 
-for nt in range(2, 10, 1):
-    for n in range(1, 11, 1):
+for nt in range(2, 10):
+    for n in range(1, 11):
         resultado = nt * n
-        print(nt, 'x', n, '=', resultado)
+        print(f'{nt} x {n} = {resultado}')
