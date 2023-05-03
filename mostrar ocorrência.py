58c05da3a8acf56d5215197544aea8747ccf5e13
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:48:22 -0300

    Update mostrar ocorrência.py

--- mostrar ocorrência.py
+++ mostrar ocorrência.py
@@ -1,23 +1,20 @@
-print('\033[34m Mostrar ocorrência\033[m')
-print()
-
 from random import randint
 
-l = []
+print('Mostrar ocorrência')
+print()
 
-for quantidade in range(100):
-    n = randint(1, 100)
-    l.append(n)
+l = [randint(1, 100) for _ in range(100)]
 
 n2 = int(input('Digite um número: '))
 print()
 
 if n2 == 5:
-    print('Esse número apareceu 2 vezez na lista.')
+    print('Esse número apareceu 2 vezes na lista.')
 elif n2 == 10:
-    print('Essse número apareceu 5 vezes na lista.')
-
-if l.count(n2) >> 0:
-    print('Esse número está na lista.', l.count(n2))
+    print('Esse número apareceu 5 vezes na lista.')
 else:
-    print('Esse número não está na lista.')
+    count = l.count(n2)
+    if count > 0:
+        print(f'Esse número apareceu {count} vezes na lista.')
+    else:
+        print('Esse número não está na lista.')
