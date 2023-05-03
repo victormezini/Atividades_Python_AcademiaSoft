3994ae7fc389095bbf602f57a1aa5393d16ee89b
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:46:24 -0300

    Update huffmancodding.py

--- huffmancodding.py
+++ huffmancodding.py
@@ -9,12 +9,10 @@ class HeapNode:
         self.left = None
         self.right = None
 
-    def __cmp__(self, other):
-        if (other == None):
-            return -1
-        if (not isinstance(other, HeapNode)):
-            return -1
-        return self.freq > other.freq
+    def __lt__(self, other):
+        if not isinstance(other, HeapNode):
+            return NotImplemented
+        return self.freq < other.freq
 
 
 class HuffmanCoding:
@@ -24,14 +22,10 @@ class HuffmanCoding:
         self.codes = {}
         self.reverse_mapping = {}
 
-    # functions for compression:
-
     def make_frequency_dict(self, text):
         frequency = {}
         for character in text:
-            if not character in frequency:
-                frequency[character] = 0
-            frequency[character] += 1
+            frequency[character] = frequency.get(character, 0) + 1
         return frequency
 
     def make_heap(self, frequency):
@@ -40,25 +34,21 @@ class HuffmanCoding:
             heapq.heappush(self.heap, node)
 
     def merge_nodes(self):
-        while (len(self.heap) > 1):
+        while len(self.heap) > 1:
             node1 = heapq.heappop(self.heap)
             node2 = heapq.heappop(self.heap)
-
             merged = HeapNode(None, node1.freq + node2.freq)
             merged.left = node1
             merged.right = node2
-
             heapq.heappush(self.heap, merged)
 
     def make_codes_helper(self, root, current_code):
-        if (root == None):
+        if root is None:
             return
-
-        if (root.char != None):
+        if root.char is not None:
             self.codes[root.char] = current_code
             self.reverse_mapping[current_code] = root.char
             return
-
         self.make_codes_helper(root.left, current_code + "0")
         self.make_codes_helper(root.right, current_code + "1")
 
@@ -75,18 +65,14 @@ class HuffmanCoding:
 
     def pad_encoded_text(self, encoded_text):
         extra_padding = 8 - len(encoded_text) % 8
-        for i in range(extra_padding):
-            encoded_text += "0"
-
-        padded_info = "{0:08b}".format(extra_padding)
-        encoded_text = padded_info + encoded_text
-        return encoded_text
+        if extra_padding == 8:
+            extra_padding = 0
+        padded_info = format(extra_padding, '08b')
+        return padded_info + encoded_text + '0' * extra_padding
 
     def get_byte_array(self, padded_encoded_text):
-        if (len(padded_encoded_text) % 8 != 0):
-            print("Encoded text not padded properly")
-            exit(0)
-
+        if len(padded_encoded_text) % 8 != 0:
+            raise ValueError("Encoded text not padded properly")
         b = bytearray()
         for i in range(0, len(padded_encoded_text), 8):
             byte = padded_encoded_text[i:i + 8]
@@ -97,9 +83,8 @@ class HuffmanCoding:
         filename, file_extension = os.path.splitext(self.path)
         output_path = filename + ".bin"
 
-        with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
-            text = file.read()
-            text = text.rstrip()
+        with open(self.path, 'r') as file, open(output_path, 'wb') as output:
+            text = file.read().rstrip()
 
             frequency = self.make_frequency_dict(text)
             self.make_heap(frequency)
@@ -115,49 +100,10 @@ class HuffmanCoding:
         print("Compressed")
         return output_path
 
-    """ functions for decompression: """
-
     def remove_padding(self, padded_encoded_text):
         padded_info = padded_encoded_text[:8]
         extra_padding = int(padded_info, 2)
-
         padded_encoded_text = padded_encoded_text[8:]
-        encoded_text = padded_encoded_text[:-1 * extra_padding]
+        return padded_encoded_text[:-extra_padding]
 
-        return encoded_text
-
-    def decode_text(self, encoded_text):
-        current_code = ""
-        decoded_text = ""
-
-        for bit in encoded_text:
-            current_code += bit
-            if (current_code in self.reverse_mapping):
-                character = self.reverse_mapping[current_code]
-                decoded_text += character
-                current_code = ""
-
-        return decoded_text
-
-    def decompress(self, input_path):
-        filename, file_extension = os.path.splitext(self.path)
-        output_path = filename + "_decompressed" + ".txt"
-
-        with open(input_path, 'rb') as file, open(output_path, 'w') as output:
-            bit_string = ""
-
-            byte = file.read(1)
-            while (byte != ""):
-                byte = ord(byte)
-                bits = bin(byte)[2:].rjust(8, '0')
-                bit_string += bits
-                byte = file.read(1)
-
-            encoded_text = self.remove_padding(bit_string)
-
-            decompressed_text = self.decode_text(encoded_text)
-
-            output.write(decompressed_text)
-
-        print("Decompressed")
-        return output_path
+    def decode_text(self,
