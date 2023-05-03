28c5a1f26941bbe0d6471f373822ffd658441e1a
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:22:19 -0300

    Update Sequencia do Fibonacci.py

--- Sequencia do Fibonacci.py
+++ Sequencia do Fibonacci.py
@@ -1,12 +1,10 @@
-
-f = 0
-n = 1
-na = 0
-
 print('\033[7mSequencia do Fibonacci\033[m')
-for jogando in range (0,10,1):
-    jogando = jogando + 1
+
+na = 0
+n = 1
+
+for i in range(10):
     f = n + na
     na = n
     n = f
-    print (f)
+    print(f)
