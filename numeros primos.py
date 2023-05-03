5b24e4eb44fa44595362c71fd4d8090adfb3400a
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:49:13 -0300

    Update numeros primos.py

--- numeros primos.py
+++ numeros primos.py
@@ -1,6 +1,14 @@
-from sys import exit
-n1 = int(input('Digite um numero inteiro: '))
-for n2 in range(2, n1):
+n1 = int(input('Digite um número inteiro: '))
+
+# Verifica se o número é menor ou igual a 1, que não é primo
+if n1 <= 1:
+    print('Este número não é primo.')
+    exit()
+
+# Verifica se o número é divisível por algum número entre 2 e a raiz quadrada dele
+for n2 in range(2, int(n1 ** 0.5) + 1):
     if n1 % n2 == 0:
-        exit('Este numero não é primo')
-print('Este numero é primo')
+        print('Este número não é primo.')
+        exit()
+
+print('Este número é primo.')
