1329fd32d44e40aa0e229805b7b512f0ea8f2808
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:12:18 -0300

    Update Lista de números aleatórios.py

--- Lista de números aleatórios.py
+++ Lista de números aleatórios.py
@@ -1,12 +1,7 @@
-print('\033[34m lista de números aleatórios\033[m')
+print('\033[34mLista de números aleatórios\033[m')
 print()
 
 from random import randint
 
-l=[]
-
-for y in range(100):
-    t = randint(1, 100)
-    l.append(t)
-
+l = [randint(1, 100) for _ in range(100)]
 print(l)
