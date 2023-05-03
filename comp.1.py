0bcb025eb44286b6fc57646f88230983001f9329
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:30:04 -0300

    Update comp.1.py

--- comp.1.py
+++ comp.1.py
@@ -1,138 +1,55 @@
-file = open ("./olamundo.txt", "r")
-
-file = file.read()
-
-afile = file.read()
-
-file2 = open ("./olamundo2.txt", "w")
-
-file3 = open ("./olamundo3.txt", "w")
-
-i=0
-strin=""
-
-lst = []
-for line in afile:
-       for ch in line:
-           lst.append(ch)
-
-cod = len(lst)
-d=0
-
-while d <cod:
-    if lst[d] == " ":
-            i+=l
-
-    elif i<0:
-        strin+="b"
-        strin+=str(i)
-        i=0
-
-     if lst[d] != " ":
-            strin+=lst[d]
-    d+=1
-print (strin)
-
-file2.write(strin)
-
-lst2=[]
-
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-    for ch in strin:
-            hi = ch
-            ho = is_number(hi)
-            if hi == "b":
-                    print ("oi")
-            elif ho==True:
-                    hu= int(hi)
-                    lst2.append/" "*(ch)
-
-res= str(lst2)
-
-we=0
-wo = len(lst2)
-res=""
-while we < wo:
-         rest+=lst2[we]
-         we+=1
-
-file3.write(res)
-
-file.close()
-file2.close()
-file3.close()
-
-return (resultado)
-
-def descompressao(string):
-    resultado = ''
-    contador = 0
-    while contador < len(string):
-        if string[contador] == 's':
-            resultado = resultado * int(string[contador + 1])
-            contador +=2
-        else:
-            resultado = resultado + string[contador]
-            contador += 1
-    return (resultado)
-
-criar('sup.txt')
-linhas = leitura('supressao.txt')
-for l in linhas:
-    escrever('sup.txt', compressao(l))
-
-criar('supressao1.txt')
-linhas = leitura('supressao.txt')
-for 1 in linhas:
-    escrever('supressao1.txt', descompressao(1))
-
-def sequencia(apontador, string):
-    contador = 0
-    caractere = string[apontador]
-    i = 0
-    while string[apontador + i] == caractere:
-        contador +=1
-        i +=1
-        if apontador + i == len(string):
-            break
-    return(contador, caractere)
-
 def compressao(string):
     apontador = 0
     resultado = ''
     while apontador < len(string):
         contador, res_temp = sequencia(apontador, string)
         if contador > 4:
+            resultado += 's' + str(contador) + res_temp
         else:
-            resultado + resultado + contador *res_temp
-        apontador +=contador
-        return (resultado)
+            resultado += contador * res_temp
+        apontador += contador
+    return resultado
 
-def leitura(nome):
-    file = open(nome, 'r')
-    lista = []
-    for line in file:
-        lista.append(line.rstrip())
-    file.close()
-    return lista
-
-def criar(nome):
-    file = open(nome, 'w')
-    file.write('')
-    file.close()
-
-def escrever(nome, linhas):
-    file = open(nome, 'a')
-    file.write(linha + '\n')
-    file.close()
+def descompressao(string):
+    resultado = ''
+    contador = 0
+    while contador < len(string):
+        if string[contador] == 's':
+            resultado += int(string[contador + 1]) * string[contador + 2]
+            contador += 3
+        else:
+            resultado += string[contador]
+            contador += 1
+    return resultado
 
 def sequencia(apontador, string):
     contador = 0
     caractere = string[apontador]
     i = 0
+    while apontador + i < len(string) and string[apontador + i] == caractere:
+        contador += 1
+        i += 1
+    return contador, caractere
+
+def leitura(nome):
+    with open(nome, 'r') as file:
+        lista = file.read().splitlines()
+    return lista
+
+def criar(nome):
+    with open(nome, 'w'):
+        pass
+
+def escrever(nome, linha):
+    with open(nome, 'a') as file:
+        file.write(linha + '\n')
+
+criar('sup.txt')
+linhas = leitura('supressao.txt')
+for linha in linhas:
+    escrever('sup.txt', compressao(linha))
+
+criar('supressao1.txt')
+linhas = leitura('supressao.txt')
+for linha in linhas:
+    escrever('supressao1.txt', descompressao(linha))
