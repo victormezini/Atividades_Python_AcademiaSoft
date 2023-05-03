8bdb6f672848ab387fc48bd6eaee23a0cb6b429e
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:06:10 -0300

    Update Calculo da Média aritmética e Mediana.py

--- Calculo da Média aritmética e Mediana.py
+++ Calculo da Média aritmética e Mediana.py
@@ -1,15 +1,20 @@
-print('\033[34mCalculo da Média aritmética e Mediana\033[m')
-print()
+print('\033[34mCálculo da Média aritmética e Mediana\033[m\n')
 
 l = []
 for v in range(20):
-    n = int(input('Digite um número inteiro: '))
+    n = int(input(f'Digite o {v+1}° número inteiro: '))
     l.append(n)
 
 print(l)
 
-m = (n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n * n) / 4
-print('A média é: ', m)
+media = sum(l) / len(l)
+print(f'A média aritmética é: {media:.2f}')
 
-print()
+l.sort()
+tamanho = len(l)
+if tamanho % 2 == 0:
+    mediana = (l[tamanho//2 - 1] + l[tamanho//2]) / 2
+else:
+    mediana = l[tamanho//2]
 
+print(f'A mediana é: {mediana:.2f}')
