5f6411921dc36f9c9f7851c9cf8b4323d3f54b2c
Víctor N. Mezini <61925943+victormezini@users.noreply.github.com>
2023-05-02 22:36:10 -0300

    Update decisoes.py

--- decisoes.py
+++ decisoes.py
@@ -1,9 +1,6 @@
 temp = 20
 if temp > 17:
-    print('esta')
-    print('\033[7mcalor\033[m')
+    print('esta\n\033[7mcalor\033[m')
 else:
-    print('esta')
-    print('\033[7mfrio\033[m')
-
+    print('esta\n\033[7mfrio\033[m')
 print('fim')
